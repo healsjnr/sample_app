ba00a232976a140d36a503996591efbb65eee636
healsjnr <healsjnr@gmail.com>
2013-07-07 18:51:06 +1000

    making user model

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,7 +10,21 @@ describe "StaticPages" do
     it { should have_selector('h1', :text => 'Sample App') }
     it { should have_selector('title', :text => full_title('')) }
     it { should_not have_selector('title', :text => '| Home') } 
-    
+ 
+    it 'should have the right links on the layout' do
+      visit root_path
+      click_link "About"
+      page.should have_selector 'title', :text => full_title('About')
+      click_link "Help"
+      page.should have_selector 'title', :text => full_title('Help')
+      click_link "Contact"
+      page.should have_selector 'title', :text => full_title('Contact')
+      click_link "Home"
+      click_link "Sign up now!"
+      page.should have_selector 'title', :text => full_title('Sign Up')
+      click_link "sample app"
+      page.should have_selector 'title', :text => full_title('')   
+    end
   end
 
   describe "Help page" do
@@ -30,5 +44,5 @@ describe "StaticPages" do
     it { should have_selector('h1', :text => 'Contact') }
     it { should have_selector('title', :text => full_title('Contact')) }
   end
-  
+ 
 end
